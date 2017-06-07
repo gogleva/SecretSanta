aeef31ba92d3ecc22744bb8487b3442b1085731e
uselessylusses <gogleva.a.a@gmail.com>
2017-06-07 16:21:00 +0100

    welcome message

--- R/hi.R
+++ R/hi.R
@@ -1,4 +1,4 @@
-#' Hi fucntion
+#' Hi function
 #'
 #' This is a test fucntion
 #' @param name enter your user name here
@@ -10,4 +10,9 @@
 hi <- function(user_name) {
   print(paste("hi", user_name, "let's get started!", sep = " "))
   }
-  
+
+#message for the user generate at the moment of package build
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to SecretSanta!")
+}
+
