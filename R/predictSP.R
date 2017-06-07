72ddb9f499ddfd4b83123deb468f61da5a9fa723
uselessylusses <gogleva.a.a@gmail.com>
2017-06-07 14:57:38 +0100

    adding sample protein file as raw data for test uses

--- R/predictSP.R
+++ R/predictSP.R
@@ -1,13 +1,14 @@
 #' PredictSP function
 #'
-#' This function takes input amino acid sequences and predict signla peptides in them
+#' This function call SignalP for prediction of signal peptides for amino acid sequences
 #' @param proteins input file/data frame with proteins
+#' @param version  spcify SignalP version to run
 #' @keywords signal peptide, proteins, signal peptide prediction, HMM
 #' @export
 #' @examples
 #' predictSP()
 
-predictSP <- function(proteins) {
+predictSP <- function(proteins, version) {
   print(paste("hi", proteins, "let's get started!", sep = " "))
 }
 
