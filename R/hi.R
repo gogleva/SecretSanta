fa404e99e24d77ea33a204c9b7b7e57c79e28d5e
uselessylusses <gogleva.a.a@gmail.com>
2017-06-07 13:08:22 +0100

    template for predictSP function

--- R/hi.R
+++ R/hi.R
@@ -1,4 +1,13 @@
-hi_function <- function(user_name) {
+#' Hi fucntion
+#'
+#' This is a test fucntion
+#' @param name enter your user name here
+#' @keywords hi
+#' @export
+#' @examples
+#' hi()
+
+hi <- function(user_name) {
   print(paste("hi", user_name, "let's get started!", sep = " "))
   }
   
---
+++ R/predictSP.R
@@ -0,0 +1,13 @@
+#' PredictSP function
+#'
+#' This function takes input amino acid sequences and predict signla peptides in them
+#' @param proteins input file/data frame with proteins
+#' @keywords signal peptide, proteins, signal peptide prediction, HMM
+#' @export
+#' @examples
+#' predictSP()
+
+predictSP <- function(proteins) {
+  print(paste("hi", proteins, "let's get started!", sep = " "))
+}
+
