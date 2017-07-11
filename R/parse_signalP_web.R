325803a365d5e0bb7f02e4a767f2260c8bcdf489
uselessylusses <gogleva.a.a@gmail.com>
2017-07-11 17:03:25 +0100

    draft function to scrap SignalP web output

---
+++ R/parse_signalP_web.R
@@ -0,0 +1,19 @@
+#' parse_signalP_web function
+#'
+#' This function scraps web output of SignalP tool and returns output in short format organised in a dataframe
+#' @param some_url url used to produce the output
+#' @keywords signal peptide, proteins, signal peptide prediction, HMM, web scrapping
+#' @export
+#' @examples
+#' parse_signalP_web()
+
+parse_signalP_web <- function(some_url){
+  webpage <- xml2::read_html(some_url)
+  raw_data_html <- rvest::html_nodes(webpage, 'pre')
+  data <- rvest::html_text(raw_data_html)
+  raw <- unlist(strsplit(data, '\n'))
+  le <- length(raw)
+  d <- raw[9:le - 4] #draft parameters
+  d2 <- as.data.frame(do.call("rbind", d2)) 
+  d2
+}
