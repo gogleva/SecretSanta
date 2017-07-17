67d07233bb53e168239bc45e443174af6ef0eccc
uselessylusses <gogleva.a.a@gmail.com>
2017-07-17 17:06:13 +0100

    remove signalp 4.0 version from the option list

--- R/run_signalp.R
+++ R/run_signalp.R
@@ -2,12 +2,11 @@
 #'
 #' This function calls local SignalP. 
 #' Please ensure that respective version of SignalP is downloaded, installed and respective path is added to $PATH variable.
-#' For details see ... 
+#' For details see ... explain with aliases and examples
 #' @param proteins input file/data frame with proteins
 #' @param version  specify SignalP version to run; versions available: \cr
 #'                 2.0 - http://www.cbs.dtu.dk/services/SignalP-2.0/ \cr
 #'                 3.0 - http://www.cbs.dtu.dk/services/SignalP-3.0/ \cr
-#'                 4.0 - http://www.cbs.dtu.dk/services/SignalP-4.0/ \cr
 #'                 4.1 - http://www.cbs.dtu.dk/services/SignalP-4.1/ \cr
 #' @param organism_type euk, gram+, gram-                 
 #' @export
