af7576ca710c6d6dfc5c78b505d4882a6ba32000
uselessylusses <gogleva.a.a@gmail.com>
2017-07-18 14:50:31 +0100

    draft workflow on SecretSanta usage - manual template

---
+++ SecretSanta_workflow.R
@@ -0,0 +1,13 @@
+### SecretSanta workflow----
+### Install signalPs (multiple versions if required) -----
+### Put paths in the separate tab-delimeted file and upload them using manage_paths()----
+manage_paths('path file') #this will leave in the environment
+### Run signalp
+
+
+
+
+
+
+
+
