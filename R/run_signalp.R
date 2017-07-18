5a5fc45d9d3357dc2405de954babaab076bf61ee
uselessylusses <gogleva.a.a@gmail.com>
2017-07-18 10:16:00 +0100

    revelation: bash aliases work only in bash, not in R

--- R/run_signalp.R
+++ R/run_signalp.R
@@ -16,11 +16,11 @@
 signalp <- function(proteins, version, organism_type) {
   message("running signalP locally...")
   signalp_version <- paste('signalp', version, sep = '')
-  print(signalp_version)
-  print(paste(signalp_version,
-              "-t",
-              organism_type,
-              proteins))
+  #print(signalp_version)
+  #print(paste(signalp_version,
+   #           "-t",
+    #          organism_type,
+     #         proteins))
   result <- tibble::as.tibble(read.table(text = (system(paste(signalp_version,
                                                       "-t",
                                                       organism_type,
