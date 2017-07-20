c68ed31c49777010b3a6e42595988cbf06e1463b
uselessylusses <gogleva.a.a@gmail.com>
2017-07-20 14:18:39 +0100

    maxC: behave diffrently for short and long numbers

--- R/parse_signalp.R
+++ R/parse_signalp.R
@@ -7,11 +7,18 @@
 #' parse_signalp(some_param)
 
 parse_signalp <- function(signalp_output) {
+  # helper function for gene ids
   clean_geneids <- function(x) {gsub('>', '', unlist(str_split(x, " "))[1])}
+  # helper function for C-score
+  clean_maxC <- function(x) {as.numeric(unlist(str_split(x, "  "))[c(3,6)])}
+  # read data
   data <-  readLines("SecretSanta/inst/extdata/sample_prot_signalp2_out")
+  # extract gene ids
   gene_ids <- data[(grep("# Measure  Position  Value  Cutoff  signal peptide?", data) - 1)]
-  gene_ids_fixed <- (sapply(gene_ids, gene_id_rep, USE.NAMES = FALSE))
-  
+  gene_ids_fixed <- (sapply(gene_ids, clean_geneids, USE.NAMES = FALSE))
+  # extract max C score and position
+  max_C <- data[grep("max. C", data)]  
+  max_C_fixed <- (sapply(max_C, clean_maxC, USE.NAMES = FALSE))
 }
 
 
