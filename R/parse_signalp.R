a29e72d1d71d819a06579a3d8dd83e046c0536dd
uselessylusses <gogleva.a.a@gmail.com>
2017-07-20 16:24:41 +0100

    parse function returns prediction status and filters entries predicted to contain signal peptide

--- R/parse_signalp.R
+++ R/parse_signalp.R
@@ -13,6 +13,8 @@ parse_signalp <- function(data_path) {
   clean_score <- function(x) {as.numeric(strsplit(x, "\\s+")[[1]][c(4,5 )])}
   # helper function for S mean
   clean_mean <- function(x) {strsplit(x, "\\s+")[[1]][c(4,5)]}
+  # helper fucntion for prediction result:
+  clean_status <- function(x) {gsub('Prediction: ', '', x)}
   # read data
   data <-  readLines(data_path)
   # extract gene ids
@@ -26,15 +28,20 @@ parse_signalp <- function(data_path) {
   max_S_fixed <- sapply(data[grep("max. S", data)], clean_score, USE.NAMES = FALSE)
   # extract mean S score and position
   mean_S_fixed <- sapply(data[grep("mean S", data)], clean_mean, USE.NAMES = FALSE)
+  Status_fixed <- sapply(data[grep("Prediction: ", data)], clean_status, USE.NAMES = FALSE)
   res <- as.tibble(data.frame(gene_ids_fixed,
                               t(max_C_fixed),
                               t(max_Y_fixed),
                               t(max_S_fixed),
-                              t(mean_S_fixed)))
+                              t(mean_S_fixed),
+                              Status_fixed))
   names(res) <- c("gene_id", "Cpos", "Cmax",
                   "Ypos", "Ymax", "Spos",
-                  "Smax", "Srange", "Smean")
-  return(res)
+                  "Smax", "Srange", "Smean", "Status")
+  #filter entries predicted to contain signal peptide
+  return(res %>% filter(Status == 'Signal peptide'))
 }
   
 res2 <- parse_signalp("SecretSanta/inst/extdata/sample_prot_signalp2_out")
+
+data <- readLines("SecretSanta/inst/extdata/sample_prot_signalp2_out")
