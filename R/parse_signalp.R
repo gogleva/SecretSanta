9637b08c0c3fba37c4eba8f09c9b2be2864efa7e
uselessylusses <gogleva.a.a@gmail.com>
2017-07-20 15:53:39 +0100

    formatting cleaned

--- R/parse_signalp.R
+++ R/parse_signalp.R
@@ -1,42 +1,40 @@
 #' parse_signalp function
 #'
 #' This function parses signalp2 and signalp3 output
-#' @param signalp_output  text output from signalp2 or signalp3, test: read from the file
+#' @param data_path  text output from signalp2 or signalp3, test: read from the file
 #' @export
 #' @examples
-#' parse_signalp(some_param)
+#' parse_signalp("SecretSanta/inst/extdata/sample_prot_signalp2_out")
 
 parse_signalp <- function(data_path) {
   # helper function for gene ids
   clean_geneids <- function(x) {gsub('>', '', unlist(str_split(x, " "))[1])}
   # helper function for C-score, Y-score and S-score: split line with varibale number of spaces
   clean_score <- function(x) {as.numeric(strsplit(x, "\\s+")[[1]][c(4,5 )])}
+  # helper function for S mean
   clean_mean <- function(x) {strsplit(x, "\\s+")[[1]][c(4,5)]}
   # read data
   data <-  readLines(data_path)
   # extract gene ids
   gene_ids <- data[(grep("# Measure  Position  Value  Cutoff  signal peptide?", data) - 1)]
-  gene_ids_fixed <- (sapply(gene_ids, clean_geneids, USE.NAMES = FALSE)) #fin
+  gene_ids_fixed <- (sapply(gene_ids, clean_geneids, USE.NAMES = FALSE))
   # extract max C score and position
-  max_C_fixed <- sapply(data[grep("max. C", data)], clean_score, USE.NAMES = FALSE) #fin
+  max_C_fixed <- sapply(data[grep("max. C", data)], clean_score, USE.NAMES = FALSE)
   # extract max Y score and position
-  max_Y_fixed <- sapply(data[grep("max. Y", data)], clean_score, USE.NAMES = FALSE) #fin
+  max_Y_fixed <- sapply(data[grep("max. Y", data)], clean_score, USE.NAMES = FALSE)
   # extract max S score and position
-  max_S_fixed <- sapply(data[grep("max. S", data)], clean_score, USE.NAMES = FALSE) #fin
+  max_S_fixed <- sapply(data[grep("max. S", data)], clean_score, USE.NAMES = FALSE)
   # extract mean S score and position
-  mean_S_fixed <- sapply(data[grep("mean S", data)], clean_mean, USE.NAMES = FALSE) #fin
-  res <- as.tibble(data.frame(gene_ids_fixed, t(max_C_fixed), t(max_Y_fixed), t(max_S_fixed), t(mean_S_fixed)))
+  mean_S_fixed <- sapply(data[grep("mean S", data)], clean_mean, USE.NAMES = FALSE)
+  res <- as.tibble(data.frame(gene_ids_fixed,
+                              t(max_C_fixed),
+                              t(max_Y_fixed),
+                              t(max_S_fixed),
+                              t(mean_S_fixed)))
   names(res) <- c("gene_id", "Cmax", "Cpos",
                      "Ymax", "Ypos", "Smax",
                      "Spos", "Smean")
   return(res)
 }
   
-
 parse_signalp("SecretSanta/inst/extdata/sample_prot_signalp2_out")
-
- 
-
-#split string with variable number of spaces:
-strsplit(max_C[10], "\\s+")[[1]]
-data <- readLines("SecretSanta/inst/extdata/sample_prot_signalp2_out") #now read from the file, later - update this
