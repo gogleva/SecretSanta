a16c9c8d3dbc970a2fb5271cbec0484bc73d0f97
uselessylusses <gogleva.a.a@gmail.com>
2017-07-20 13:55:39 +0100

    parse_signalp: extract the gene ids and return a vector

--- R/parse_signalp.R
+++ R/parse_signalp.R
@@ -1,19 +1,29 @@
 #' parse_signalp function
 #'
 #' This function parses signalp2 and signalp3 output
-#' @param param  some param                
+#' @param signalp_output  text output from signalp2 or signalp3, test: read from the file
 #' @export
 #' @examples
 #' parse_signalp(some_param)
 
-parse_signalp <- function(some_param) {
-  print(some_param)
+parse_signalp <- function(signalp_output) {
+  clean_geneids <- function(x) {gsub('>', '', unlist(str_split(x, " "))[1])}
+  data <-  readLines("SecretSanta/inst/extdata/sample_prot_signalp2_out")
+  gene_ids <- data[(grep("# Measure  Position  Value  Cutoff  signal peptide?", data) - 1)]
+  gene_ids_fixed <- (sapply(gene_ids, gene_id_rep, USE.NAMES = FALSE))
+  
 }
 
 
-#data <- readLines("SecretSanta/inst/extdata/sample_prot_signalp2_out")
-#data
+data <- readLines("SecretSanta/inst/extdata/sample_prot_signalp2_out") #now read from the file, later - update this
+# raw lines
 
-#data[grep(">", data)]
+max_C <- data[grep("max. C", data)]  
+max_Y <- data[grep("max. Y", data)]
+max_S <- data[grep("max. S", data)]
+mean_S <- data[grep("mean S", data)]
+
+#clean them!
+
 
 
--- SecretSanta_workflow.R
+++ SecretSanta_workflow.R
@@ -3,8 +3,7 @@
 ### Put paths in the separate tab-delimeted file and upload them using manage_paths()----
 secret_paths <- manage_paths("SecretSanta/inst/extdata/sample_paths") #this will leave in the environment
 ### Run signalp
-
-
+result <- signalp(proteins = "SecretSanta/inst/extdata/sample_prot.fasta", organism_type = 'euk', version = 4)
 
 
 
