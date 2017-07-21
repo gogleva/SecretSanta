e60caf2b0f6b07f7766377d54030d587d4874e1a
uselessylusses <gogleva.a.a@gmail.com>
2017-07-21 17:45:02 +0100

    to do: figure out how to make targetp work

--- R/run_targetP.R
+++ R/run_targetP.R
@@ -1,12 +1,13 @@
 #' tragetp function
 #'
 #' This function calls local targetp
+#' Don't forget to specify where to put tmp files! Change this by editing targetp source code
 #' @param proteins input file with proteins
 #' @param output_type what we want from the output
 #' @export
-#' @examples
-
+#' @examples 
 
 targetp <- function(proteins, output_type) {
   message("running targetp locally...")
-}
+  
+  }
