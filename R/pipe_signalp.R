fc1994383febb0abd847e53f42bb618d4b05dac2
uselessylusses <gogleva.a.a@gmail.com>
2017-07-21 14:28:01 +0100

    pipe_signalp: transform piping sequence into a standard message

--- R/pipe_signalp.R
+++ R/pipe_signalp.R
@@ -1,11 +1,33 @@
 #' pipe_signalp function
 #'
 #' This function pipes outputs from different version of signalp
-#' @param piping_sequence  specify piping sequence: what to pipe where and when
+#' @param piping_sequence  specify piping sequence: vector of signalp versions \cr
+#'                         accepted formats: c(1,2,3) or c("signalp2", "signalp3", "signalp4")
+#' @param input_fasta fasta file with proteins to initiate the pipe
 #' @export
 #' @examples
 #'
 #'
-pipe_signalp <- function(piping_sequence) {
+#'
+
+pipe_signalp <- function(piping_sequence, input_fasta) {
   message("Hi, let's pipe!")
+  if (all(stringr::str_detect(piping_sequence, "signalp"))) {
+    piping_sequence
+  } else if(is.numeric(piping_sequence)){
+    piping_sequence <- paste('signalp', piping_seq_num, sep = '')
+  }
+  message(paste(piping_sequence[-length(piping_sequence)], '--> '), piping_sequence[length(piping_sequence)])
 }
+
+
+#tests:
+
+piping_seq <- c("signalp2", "signalp3", "signalp4")
+piping_seq_num <- c(2,3,3)
+
+pipe_signalp(piping_sequence = piping_seq, 'some_fasta')
+pipe_signalp(piping_sequence = piping_seq_num, 'some_fasta')
+
+
+
