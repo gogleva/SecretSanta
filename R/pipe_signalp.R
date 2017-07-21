2a34ed722ed07ffdc1518b3dc57272e203d40770
uselessylusses <gogleva.a.a@gmail.com>
2017-07-21 14:40:13 +0100

    documnetation updated and examples for pipe_signalp included

--- R/pipe_signalp.R
+++ R/pipe_signalp.R
@@ -6,11 +6,14 @@
 #' @param input_fasta fasta file with proteins to initiate the pipe
 #' @export
 #' @examples
-#'
-#'
+#' piping_seq <- c("signalp2", "signalp3", "signalp4")
+#' piping_seq_num <- c(2,3,3)
+#' pipe_signalp(piping_sequence = piping_seq, 'some_fasta')
+#' pipe_signalp(piping_sequence = piping_seq_num, 'some_fasta')
 #'
 
 pipe_signalp <- function(piping_sequence, input_fasta) {
+  # generate starting message
   message("Hi, let's pipe!")
   if (all(stringr::str_detect(piping_sequence, "signalp"))) {
     piping_sequence
@@ -18,16 +21,8 @@ pipe_signalp <- function(piping_sequence, input_fasta) {
     piping_sequence <- paste('signalp', piping_seq_num, sep = '')
   }
   message(paste(piping_sequence[-length(piping_sequence)], '--> '), piping_sequence[length(piping_sequence)])
+  # do the piping
 }
-
-
-#tests:
-
-piping_seq <- c("signalp2", "signalp3", "signalp4")
-piping_seq_num <- c(2,3,3)
-
-pipe_signalp(piping_sequence = piping_seq, 'some_fasta')
-pipe_signalp(piping_sequence = piping_seq_num, 'some_fasta')
 
 
 
