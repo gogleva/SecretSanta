39535fbb778ccd3e48f3e1702c803c1ac4adbbcc
uselessylusses <gogleva.a.a@gmail.com>
2017-07-24 10:54:40 +0100

    major issue: targetp does not run on Ubuntu 16.04, 12.04 is fine

--- R/run_targetP.R
+++ R/run_targetP.R
@@ -1,7 +1,9 @@
 #' tragetp function
 #'
 #' This function calls local targetp
-#' Don't forget to specify where to put tmp files! Change this by editing targetp source code
+#' Please check targetp README before running this fucntion:
+#' TARGETP         'targetp-1.1' directory
+#' TMP             location for temporary data
 #' @param proteins input file with proteins
 #' @param output_type what we want from the output
 #' @export
@@ -10,4 +12,4 @@
 targetp <- function(proteins, output_type) {
   message("running targetp locally...")
   
-  }
+    }
