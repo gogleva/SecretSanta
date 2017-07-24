4d373e64ec354accc82975162c7862d9f1cd7e32
uselessylusses <gogleva.a.a@gmail.com>
2017-07-24 09:26:10 +0100

    signalp: added check for stop codon symbols in the input fasta file, if any preasent - it aborts

--- R/run_signalp.R
+++ R/run_signalp.R
@@ -22,27 +22,29 @@ signalp <- function(proteins, version, organism_type, output_type) {
   message("running signalP locally...")
   # check input fasta files for illegal characters and other incompatibilities
   
+  fasta <- readLines(proteins)
+  if (any(grepl('[*$]',fasta[!grepl('>', fasta)]))){
+    stop('input fasta file cotains stop codons "*", please remove them and re-run')
+  }else{
+    # determine which of signalp paths we need to use:
+    signalp_version <- paste("signalp", version, sep = '')
+    message(signalp_version)
+    full_pa <- as.character(secret_paths %>% filter(tool == signalp_version) %>% select(path))
   
-  
-  # determine which of signalp paths we need to use:
-  signalp_version <- paste("signalp", version, sep = '')
-  message(signalp_version)
-  full_pa <- as.character(secret_paths %>% filter(tool == signalp_version) %>% select(path))
-  
-  if (version >= 4) {
-    # running signalp versions 4 and 4.1
-    result <- tibble::as.tibble(read.table(text = (system(paste(full_pa, "-t", organism_type, proteins), intern = TRUE))))
-    names(result) <- c("gene_id", "Cmax", "Cpos",
+    if (version >= 4) {
+      # running signalp versions 4 and 4.1
+      result <- tibble::as.tibble(read.table(text = (system(paste(full_pa, "-t", organism_type, proteins), intern = TRUE))))
+      names(result) <- c("gene_id", "Cmax", "Cpos",
                        "Ymax", "Ypos", "Smax",
                        "Spos", "Smean", "D",
                        "Prediction", "Dmaxcut", "Networks-used")
-    # returns tibble for candidate secreted proteins only
-    return(result %>% filter(Prediction == 'Y'))
-  } else if(version < 4) {
-    # running signalp versions 2 and 3, call parser for the initial output
-    message('ancient signalp, calling parser for the output...')
-    con <- system(paste(full_pa, "-t", organism_type, proteins), intern = TRUE)
-    result <- parse_signalp(input = con, input_type = "system_call")
+      # returns tibble for candidate secreted proteins only
+      return(result %>% filter(Prediction == 'Y'))
+    } else if(version < 4) {
+      # running signalp versions 2 and 3, call parser for the initial output
+      message('ancient signalp, calling parser for the output...')
+      con <- system(paste(full_pa, "-t", organism_type, proteins), intern = TRUE)
+      result <- parse_signalp(input = con, input_type = "system_call")
+    }
   }
 }
-
