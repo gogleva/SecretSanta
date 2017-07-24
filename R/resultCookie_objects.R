a3d476dea89ffef640ab91d7b5b99efee86aa28b
uselessylusses <gogleva.a.a@gmail.com>
2017-07-24 16:57:28 +0100

    update contents of resultCookie class

--- R/resultCookie_objects.R
+++ R/resultCookie_objects.R
@@ -2,10 +2,16 @@
 #' secretome prediction pipeline
 #' 
 #' @slot fasta initial fasta file
-#' @slot mature_fasta fasta with mature sequences
-#' 
-#' 
+#' @slot mature_fasta   fasta with mature sequences
+#' @slot sp_tibble      standard tibble with outputs obtained from signalp
+#' @slot tmhmm_tibble   tmhmm results
+#' @slot targetp_tibble targetp results
+#' @slot er_motifs             ER motifs: (K/H)DEL
 #' 
 resultCookie <- setClass("resultCookie",
                     slots = list(fasta = "character",
-                                 mature_fasta = "character"))
+                                 mature_fasta = "character",
+                                 sp_tibble = 'tibble',
+                                 tmhmm_tibble = 'tibble',
+                                 tagetp_tibble = 'tibble',
+                                 er_motifs = 'character'))
