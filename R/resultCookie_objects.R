90becabaf71ebe96c6b033aa43f56ed1502137b3
uselessylusses <gogleva.a.a@gmail.com>
2017-07-24 17:06:53 +0100

    update structure of the resultCookie object

--- R/resultCookie_objects.R
+++ R/resultCookie_objects.R
@@ -1,17 +1,21 @@
 #' An S4 class to represent intermediate and final outputs of the
 #' secretome prediction pipeline
 #' 
-#' @slot fasta initial fasta file
-#' @slot mature_fasta   fasta with mature sequences
-#' @slot sp_tibble      standard tibble with outputs obtained from signalp
-#' @slot tmhmm_tibble   tmhmm results
-#' @slot targetp_tibble targetp results
-#' @slot er_motifs             ER motifs: (K/H)DEL
+#' @slot paths           paths to CBS exceutibles
+#' @slot fasta           initial fasta file
+#' @slot mature_fasta    fasta with mature sequences
+#' @slot sp_tibble       standard tibble with outputs obtained from signalp
+#' @slot tmhmm_tibble    tmhmm results
+#' @slot targetp_tibble  targetp results
+#' @slot er_motifs       ER motifs: (K/H)DEL
 #' 
 resultCookie <- setClass("resultCookie",
-                    slots = list(fasta = "character",
-                                 mature_fasta = "character",
+                    slots = list(paths = 'file'
+                                 fasta = "file",
+                                 mature_fasta = "file",
                                  sp_tibble = 'tibble',
                                  tmhmm_tibble = 'tibble',
                                  tagetp_tibble = 'tibble',
                                  er_motifs = 'character'))
+
+
