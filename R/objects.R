753c929e4a4ec8e03487fb80a37364ac5d03f301
uselessylusses <gogleva.a.a@gmail.com>
2017-07-25 10:56:36 +0100

    TargetpResults: defined class and slots for targetp output

--- R/objects.R
+++ R/objects.R
@@ -16,6 +16,23 @@ SignalpResult <- setClass("SignalpResult",
                                  sp_tibble = 'tbl_df')
                         )
 
+#' An S4 class to represent intermediate and final outputs of the targetp prediction step
+#' 
+#' @slot paths           paths to CBS exceutibles, do I need it here?
+#' @slot in_fasta        initial fasta file, do we need to drag it along?
+#' @slot out_fasta       output fasta with only positive candidates, i.e. not associated with cell organells
+#' @slot tp_tibble       standard tibble with outputs obtained from signalp
+#' 
+
+TargetppResult <- setClass("TargetpResult",
+                          slots = list(paths = 'file',
+                                       in_fasta = "file",
+                                       out_fasta = "file",
+                                       tp_tibble = "file",
+                                       sp_tibble = 'tbl_df')
+)
+
+
 
 
                     
