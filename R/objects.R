4ecec1667991020dc439fe9086c2eaa0a8aaa87c
uselessylusses <gogleva.a.a@gmail.com>
2017-07-25 13:41:46 +0100

    documentation updated for SignalpResult class; neat way to specifiy columns within individual slots with items

--- R/objects.R
+++ R/objects.R
@@ -1,16 +1,27 @@
 #' An S4 class to represent intermediate and final outputs of the signalp prediction step
 #' 
-#' @slot paths           paths to CBS exceutibles, do I need it here?
 #' @slot in_fasta        initial fasta file, do we need to drag it along?
 #' @slot out_fasta       output fasta with only positive candidates, i.e sequences predicted to be secreted at this step
 #' @slot mature_fasta    fasta with mature sequences
 #' @slot sp_version      version of signalp used to generate this object
-#' @slot sp_tibble       standard tibble with outputs obtained from signalp
+#' @slot sp_tibble       Object of class tibble, contains ... columns:
+#' \itemize{
+#'   \item gene_id - unique id of the sequence
+#'   \item Cmax - 
+#'   \item Cpos - 
+#'   \item Ymax - 
+#'   \item Ypos - 
+#'   \item Smax -
+#'   \item Spos -
+#'   \item Smean -
+#'   \item Prediction -
+#'   \item Dmaxcut -
+#'   \item Networks used -
+#'   }
 #' 
 
 SignalpResult <- setClass("SignalpResult",
-                    slots = list(paths = "file",
-                                 in_fasta = "file",
+                    slots = list(in_fasta = "file",
                                  out_fasta = "file",
                                  mature_fasta = "file",
                                  sp_tibble = "tbl_df")
--- R/run_targetP.R
+++ R/run_targetP.R
@@ -8,6 +8,7 @@
 #' @param output_type what we want from the output
 #' @export
 #' @examples 
+#' 
 
 targetp <- function(proteins, output_type) {
   message("running targetp locally...")
