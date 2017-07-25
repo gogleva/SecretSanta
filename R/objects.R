92db425055ef93d2a3e9c89a9c140396bb816567
uselessylusses <gogleva.a.a@gmail.com>
2017-07-25 11:03:26 +0100

    TMHMMResults: defined class and slots for targetp output

--- R/objects.R
+++ R/objects.R
@@ -21,7 +21,7 @@ SignalpResult <- setClass("SignalpResult",
 #' @slot paths           paths to CBS exceutibles, do I need it here?
 #' @slot in_fasta        initial fasta file, do we need to drag it along?
 #' @slot out_fasta       output fasta with only positive candidates, i.e. not associated with cell organells
-#' @slot tp_tibble       standard tibble with outputs obtained from signalp
+#' @slot tp_tibble       standard tibble with outputs obtained from targetp
 #' 
 
 TargetppResult <- setClass("TargetpResult",
@@ -32,6 +32,25 @@ TargetppResult <- setClass("TargetpResult",
                                        sp_tibble = 'tbl_df')
 )
 
+
+#' An S4 class to represent intermediate and final outputs of the TMHMM prediction step
+#' 
+#' @slot paths            paths to CBS exceutibles, do I need it here?
+#' @slot in_fasta         initial fasta file
+#' @slot in_mature_fasta  input mature fasta, 
+#' @slot out_fasta        output fasta with only positive full-length candidates, candidates without TM in mature sequences
+#' @slot out_mature_fasta output mature, conatins mature sequences without TM domains
+#' @slot tm_tibble        tibble with outputs obtained from TMHMM
+#' 
+
+TargetppResult <- setClass("TargetpResult",
+                           slots = list(paths = 'file',
+                                        in_fasta = "file",
+                                        in_mature_fasta = "file",
+                                        out_fasta = "file",
+                                        tp_tibble = "file",
+                                        sp_tibble = 'tbl_df')
+)
 
 
 
