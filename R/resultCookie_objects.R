168abda43b766f0005a62fcf58bd7b81db2e0678
uselessylusses <gogleva.a.a@gmail.com>
2017-07-25 08:56:05 +0100

    documentation updated, objects; alternative solution - have a separate class for each type of output (comming form each tool); objects will communicate by passing fasta files to the respective runers

--- R/resultCookie_objects.R
+++ R/resultCookie_objects.R
@@ -1,7 +1,7 @@
 #' An S4 class to represent intermediate and final outputs of the
 #' secretome prediction pipeline
 #' 
-#' @slot paths           paths to CBS exceutibles
+#' @slot paths           paths to CBS exceutibles, do I need it here?
 #' @slot fasta           initial fasta file
 #' @slot mature_fasta    fasta with mature sequences
 #' @slot sp_tibble       standard tibble with outputs obtained from signalp
@@ -9,13 +9,17 @@
 #' @slot targetp_tibble  targetp results
 #' @slot er_motifs       ER motifs: (K/H)DEL
 #' 
+#' 
 resultCookie <- setClass("resultCookie",
-                    slots = list(paths = 'file'
+                    slots = list(paths = 'file',
                                  fasta = "file",
                                  mature_fasta = "file",
-                                 sp_tibble = 'tibble',
-                                 tmhmm_tibble = 'tibble',
-                                 tagetp_tibble = 'tibble',
-                                 er_motifs = 'character'))
+                                 sp_tibble = 'tbl_df',
+                                 tmhmm_tibble = 'tbl_df',
+                                 tagetp_tibble = 'tbl_df',
+                                 er_motifs = 'character')
+                        )
+
+
 
 
