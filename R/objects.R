db7c56737c7613d020f80989bab0f7196875f7c3
uselessylusses <gogleva.a.a@gmail.com>
2017-07-26 13:46:21 +0100

    issue with sp_version validity check and accessor functions

--- R/objects.R
+++ R/objects.R
@@ -49,8 +49,8 @@ SignalpResult <- setClass(
                     {
                              if (length(object@in_fasta) < length(object@out_fasta)) {
                               return("Number of output sequences is grater than the number of input sequences.")
-                             } else if ((sp_version < 2 | sp_version >= 5)) {
-                              return("signalp version is invalid") 
+                            # } else if (!(sp_version < 2 | sp_version >= 5)) { 
+                            #  return("signalp version is invalid") 
                              } else if (any(grepl('[*$]', object@in_fasta))) {
                               return("Input fasta contains stop codon symbols '*', please remove them.") 
                              }
