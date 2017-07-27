ec05820505d67e00029796680088906e0bb562d9
uselessylusses <gogleva.a.a@gmail.com>
2017-07-27 15:24:16 +0100

    tmhmm now returns CBSResult object as an output

--- R/run_TMHMM.R
+++ R/run_TMHMM.R
@@ -14,23 +14,28 @@
 
 # to do: make this function to produce an object of CBSResult class
 
-step1_sp2 # obj of SignalpResult class
-
 tmhmm <- function(input_obj) {
   message("running TMHMM locally...")
   
-  
+  fasta <- getInfasta(input_obj) #placeholder, should be suitable for objects with mature_fasta slots only
+  out_tmp <- tempfile()
+  Biostrings::writeXStringSet(fasta, out_tmp)
   
   full_pa <- as.character(secret_paths %>% filter(tool == 'tmhmm') %>% select(path))
-  result <- tibble::as.tibble(read.table(text = (system(paste(full_pa, proteins, '--short'), intern = TRUE))))
+  result <- tibble::as.tibble(read.table(text = (system(paste(full_pa, out_tmp, '--short'), intern = TRUE))))
   names(result) <- c("gene_id", "length", "ExpAA",
                      "First60", "PredHel", "Topology")
   result <- (result %>% filter(PredHel == 'PredHel=0'))
   
-  
-}
+  out_obj <- TMhmmResult(in_fasta = fasta,
+                         out_fasta = fasta, #placeholder
+                         tm_tibble = result)
+  if (validObject(out_obj)) {return(out_obj)}
+  }
 
 ### tests
 
-t1 <- tmhmm("SecretSanta/inst/extdata/sample_prot.fasta", 'test')
+#t1 <- tmhmm("SecretSanta/inst/extdata/sample_prot.fasta", 'test') # old version
+
+t2 <- tmhmm(step1_sp2) # obj of SignalpResult class
 
