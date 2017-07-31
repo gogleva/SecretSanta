99c82e44281749c18f5b8adc894070619a2b4be9
uselessylusses <gogleva.a.a@gmail.com>
2017-07-31 11:04:33 +0100

    workflow updated, piping between tools works ok

--- SecretSanta_workflow.R
+++ SecretSanta_workflow.R
@@ -2,15 +2,35 @@
 ### Install signalPs (multiple versions if required) -----
 ### Put paths in the separate tab-delimeted file and upload them using manage_paths()----
 secret_paths <- manage_paths("SecretSanta/inst/extdata/sample_paths") #this will leave in the environment
-### Run signalp
-result <- signalp(proteins = "SecretSanta/inst/extdata/sample_prot.fasta", organism_type = 'euk', version = 4)
-result2 <- signalp(proteins = "SecretSanta/inst/extdata/sample_prot.fasta", organism_type = 'euk', version = 2)
 
-### Parse signalP2/3 output:
-res2 <- parse_signalp("SecretSanta/inst/extdata/sample_prot_signalp2_out", input_type = "path")
+### Example pipe pipeline:
 
-### run tmhmm:
+#initialise SignalpResult object
+inp <- SignalpResult()
 
-#tmhmm()
+#read fasta file in AAStringSet object
+aa <- readAAStringSet("SecretSanta/inst/extdata/sample_prot.fasta", use.names = TRUE)
+
+#assign this object to the input_fasta slot of SignalpResult object
+inp <- setInfasta(inp, aa)
+
+#run signalp2 on the initial file:
+step1_sp2 <- signalp(inp, version = 2, 'euk', run_mode = "starter")
+
+#run signalp3 on the result object, will automatically pass out_fasta slot to signalp3:
+step2_sp3 <- signalp(step1_sp2, version = 3, 'euk', run_mode = "piper")
+
+#run signalp4 on the result object, will automatically pass out_fasta slot to signalp4:
+step3_sp4 <- signalp(step2_sp3, version = 4, 'euk', run_mode = "piper")
+
+#run TMHMM on the previous step
+
+step4_TMHMM <- tmhmm(step3_sp4)
+
+#check termial ER retention signals
+
+step5_ER <- check_khdel(step4_TMHMM, 'piper')
+
+# predict subcellular localisation (wolf psort)
 
 
