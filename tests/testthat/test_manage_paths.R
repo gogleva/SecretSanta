a82110db0db4d76c21d6ea8423ad1a3430fe5891
uselessylusses <gogleva.a.a@gmail.com>
2017-08-01 11:42:08 +0100

    unit tests simplified

--- tests/testthat/test_manage_paths.R
+++ tests/testthat/test_manage_paths.R
@@ -2,21 +2,17 @@ context("Check external dependencies")
 
 test_that("manage_paths returns correct object and messages",
           {
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
-                         'All paths are valid')
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
-                           'signalp2 test run completed')
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
-                         'signalp3 test run completed')
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
-                         'signalp4 test run completed')
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
-                         'targetp test run completed')
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
-                         'tmhmm test run completed')
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
-                         'wolfpsort test run completed')
-          result <- manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta"))
+          s_file <- system.file("extdata", "sample_paths", package = "SecretSanta")
+          
+          expect_message(manage_paths(s_file), 'All paths are valid')
+          expect_message(manage_paths(s_file), 'signalp2 test run completed')
+          expect_message(manage_paths(s_file), 'signalp3 test run completed')
+          expect_message(manage_paths(s_file), 'signalp4 test run completed')
+          expect_message(manage_paths(s_file), 'targetp test run completed')
+          expect_message(manage_paths(s_file), 'tmhmm test run completed')
+          expect_message(manage_paths(s_file), 'wolfpsort test run completed')
+          
+          result <- manage_paths(s_file)
           expect_is(result, c('tbl_df', 'tbl'))
           expect_equal(ncol(result), 3)
           })
