2aea5dcbe9fae3fba5848431a593f33466883324
uselessylusses <gogleva.a.a@gmail.com>
2017-08-01 11:36:59 +0100

    added tests for individual tool messages

--- tests/testthat/test_manage_paths.R
+++ tests/testthat/test_manage_paths.R
@@ -1,9 +1,21 @@
-context("Check paths")
+context("Check external dependencies")
 
 test_that("manage_paths returns correct object and messages",
           {
           expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
                          'All paths are valid')
+          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
+                           'signalp2 test run completed')
+          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
+                         'signalp3 test run completed')
+          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
+                         'signalp4 test run completed')
+          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
+                         'targetp test run completed')
+          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
+                         'tmhmm test run completed')
+          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
+                         'wolfpsort test run completed')
           result <- manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta"))
           expect_is(result, c('tbl_df', 'tbl'))
           expect_equal(ncol(result), 3)
