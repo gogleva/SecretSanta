28f4637bac6e9b2f18d9f1b21302d362a320c9a4
uselessylusses <gogleva.a.a@gmail.com>
2017-08-01 10:43:53 +0100

    added internal checks for signalp dependencies; tools can be executed in principle and return help messages

--- R/manage_paths.R
+++ R/manage_paths.R
@@ -37,24 +37,55 @@ manage_paths <- function(path_file) {
     message('All paths are valid')
     # convert all the tool names to lower case to avoid confusion
     pp <- plyr::mutate(pp, tool = tolower(tool))
-    return(pp)
     } else {
     message('Supplied file path does not exist.')
     message(sapply(pp %>% dplyr::filter(status == FALSE) %>% dplyr::select(path), paste, '\n'))
     message('Please check that supplied paths are correct and try again.')
     }
   
-  if
+  # check that all the dependencies are executable in principle, i.e we are able to run -h or
+  # process a small sample fasta file
   
-  # check that all the dependencies are executable in principle, i.e we are able to run -h
+  my_tools <- c('signalp2', 'signalp3', 'signalp4', 'targetp', 'tmhmm', 'wolfpsort')
+  get_paths <- function(tool_name) {pp %>% filter(tool == tool_name) %>% select(path)}
+  
+  # siganlp2:
+  
+  if (suppressWarnings(system(paste(get_paths('signalp2'), '-h'), intern = TRUE)[2]) ==
+      'Usage: signalp -t euk|gram+|gram- [options] seqfile') 
+      { message('signalp2 test run completed.')
+  } else {
+        message('signalp2 test run failed; check if it is installed correctly.')
+  }
+  
+  # signalp3:
+  
+  if (suppressWarnings(system(paste(get_paths('signalp3'), '-h'), intern = TRUE)[2]) ==
+      'Usage: signalp -t euk|gram+|gram- [options] seqfile') 
+      { message('signalp3 test run completed.')
+  } else {
+        message('signalp3 test run failed; check if it is installed correctly.')
+  }
+  
+  # signalp4
+  
+  if (suppressWarnings(system(paste(get_paths('signalp4'), '-h'), intern = TRUE)[2]) ==
+      '  Description: Predict signal peptide and cleavage site.') 
+     { message('signalp4 test run completed.')
+  } else {
+       message('signalp4 test run failed; check if it is installed correctly.')
+  }
+  
+  
+ 
+  return(pp)
   
 }
 
+
+
 pa <- manage_paths(system.file("extdata", "sample_paths", package="SecretSanta"))
-
 expect_match((system(paste((pa$path[1]), '-h'))), 'Description: Predict signal peptide and cleavage site.')
-
-
 t1 <- system(paste((pa$path[1]), '-h'), intern = TRUE)[2]
 
 #signalp4:
@@ -62,5 +93,15 @@ t1 <- system(paste((pa$path[1]), '-h'), intern = TRUE)[2]
 expect_match(t1[2], 'Description: Predict signal peptide and cleavage site.')
 
 #signalp2:
+signalp2
+
+my_tools <- c('signalp2', 'signalp3', 'signalp4', 'targetp', 'tmhmm', 'wolfpsort')
+get_paths <- function(tool_name) {pa %>% filter(tool == tool_name) %>% select(path)}
+
+sapply(my_tools, get_paths)
+  
+pa %>% filter(tool == 'signalp2') %>% select(path)
+
+system(paste(get_paths('signalp2'), '-h'))
 
 
