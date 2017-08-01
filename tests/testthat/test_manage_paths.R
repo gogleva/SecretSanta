01cefce988a6b43cf0f97e03313080b88db97c4c
uselessylusses <gogleva.a.a@gmail.com>
2017-08-01 09:08:23 +0100

    added unit_test for manage_paths function: message and return object

--- tests/testthat/test_manage_paths.R
+++ tests/testthat/test_manage_paths.R
@@ -1,7 +1,12 @@
 context("Check paths")
 
-test_that("manage_paths fucntion works as expected",
+test_that("manage_paths function works as expected",
           {
-          expect_message(manage_paths(system.file("extdata", "sample_paths", package="SecretSanta")),
-                       'All paths are valid')  
+          expect_message(manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta")),
+                           'All paths are valid')
+          
+          result <- manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta"))
+          expect_is(result,
+                    c('tbl_df', 'tbl'))
+          
           })
