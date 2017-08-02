4d1b7ae7c0c66c340ae830933fc118647318e51d
uselessylusses <gogleva.a.a@gmail.com>
2017-08-02 10:02:26 +0100

    minor changes in function formating and documentation

--- R/run_signalp.R
+++ R/run_signalp.R
@@ -65,55 +65,57 @@ signalp <- function(input_obj, version, organism_type, run_mode, paths) {
     stop('Input signalp version or specified organism type are invalid.')  
   }
     
-    # get fasta from SignalpResult object
+  # get fasta from SignalpResult object
     
-    if (run_mode == 'piper') fasta <- getOutfasta(input_obj) else fasta <- getInfasta(input_obj)  
-    #fasta <- getInfasta(input_obj) # should getOutfasta be default option?
-    # convert it to a temporary file:
-    out_tmp <- tempfile() #create a temporary file for fasta
-    Biostrings::writeXStringSet(fasta, out_tmp) #write tmp fasta file
-    # make a system call of signalp based on the tmp file
-    signalp_version <- paste("signalp", version, sep = '')
-    message(signalp_version)
-    full_pa <- as.character(paths %>% filter(tool == signalp_version) %>% select(path))
+  if (run_mode == 'piper') fasta <- getOutfasta(input_obj) else fasta <- getInfasta(input_obj)  
+  
+  # convert it to a temporary file:
+  out_tmp <- tempfile() #create a temporary file for fasta
+  Biostrings::writeXStringSet(fasta, out_tmp) #write tmp fasta file
+  
+  # make a system call of signalp based on the tmp file
+  
+  signalp_version <- paste("signalp", version, sep = '')
+  message(signalp_version)
+  full_pa <- as.character(paths %>% filter(tool == signalp_version) %>% select(path))
     
-    # helper function: crop long names for AAStringSet object, return character vector
-    crop_names <- function(x){unlist(stringr::str_split(x, " "))[1]}
+  # helper function: crop long names for AAStringSet object, return character vector
+  crop_names <- function(x){unlist(stringr::str_split(x, " "))[1]}
     
-    # ----
-    if (version >= 4) {
-    # runing signalp versios 4 and 4.1    
-      sp <- tibble::as.tibble(read.table(text = (system(paste(full_pa, "-t", organism_type, out_tmp), intern = TRUE))))
-      names(sp) <- c("gene_id", "Cmax", "Cpos",
+  # ----
+  if (version >= 4) {
+  # runing signalp versios 4 and 4.1    
+    sp <- tibble::as.tibble(read.table(text = (system(paste(full_pa, "-t", organism_type, out_tmp), intern = TRUE))))
+    names(sp) <- c("gene_id", "Cmax", "Cpos",
                             "Ymax", "Ypos", "Smax",
                             "Spos", "Smean", "D",
                             "Prediction", "Dmaxcut", "Networks-used")
-      sp <- sp %>% filter(Prediction == 'Y')
+    sp <- sp %>% filter(Prediction == 'Y')
       
-    } else if (version < 4) {
-    # running signalp versions 2 and 3, call parser for the initial output
+  } else if (version < 4) {
+  # running signalp versions 2 and 3, call parse_signalp for the output
       message('signalp < 4, calling parser for the output...')
       con <- system(paste(full_pa, "-t", organism_type, out_tmp), intern = TRUE)
       sp <- parse_signalp(input = con, input_type = "system_call")
     }
 
-    #generate cropped names for input fasta
-    cropped_names <- unname(sapply(names(fasta), crop_names))
-    #replace long names with cropped names
-    names(fasta) <- cropped_names
-    #get ids of candidate secreted proteins
-    candidate_ids <- sp %>% select(gene_id) %>% unlist(use.names = FALSE)
-    out_fasta_sp <- fasta[candidate_ids]
+  # generate cropped names for input fasta
+  cropped_names <- unname(sapply(names(fasta), crop_names))
+  # replace long names with cropped names
+  names(fasta) <- cropped_names
+  # get ids of candidate secreted proteins
+  candidate_ids <- sp %>% select(gene_id) %>% unlist(use.names = FALSE)
+  out_fasta_sp <- fasta[candidate_ids]
     
-    #generate mature sequences
+  # generate mature sequences
     
-    sp_Cpos <- sp %>% select(Cpos) %>% unlist(use.names = FALSE)
-    cropped_fasta <- subseq(out_fasta_sp, start = sp_Cpos, end = -1)
+  sp_Cpos <- sp %>% select(Cpos) %>% unlist(use.names = FALSE)
+  cropped_fasta <- subseq(out_fasta_sp, start = sp_Cpos, end = -1)
     
-    out_obj <- SignalpResult(in_fasta = fasta,
-                             out_fasta = out_fasta_sp, 
-                             mature_fasta = cropped_fasta, 
-                             sp_version = version,
-                             sp_tibble = sp)
-    if (validObject(out_obj)) {return(out_obj)}
+  out_obj <- SignalpResult(in_fasta = fasta,
+                           out_fasta = out_fasta_sp, 
+                           mature_fasta = cropped_fasta, 
+                           sp_version = version,
+                           sp_tibble = sp)
+  if (validObject(out_obj)) {return(out_obj)}
 }
