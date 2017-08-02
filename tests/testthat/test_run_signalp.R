28b0eba7006177c7544fb07db27d065d918e21c6
uselessylusses <gogleva.a.a@gmail.com>
2017-08-02 14:43:14 +0100

    create validity function for SignalpResult class instead of unit tests on output object, if SpRecult object is returned it must be valid

--- tests/testthat/test_run_signalp.R
+++ tests/testthat/test_run_signalp.R
@@ -64,8 +64,3 @@ test_that("signalp correctly responds to invalid inputs",
           expect_error(suppressMessages(signalp(inp, version = 3, 'gram-', run_mode = "start", paths = my_pa)), 
                        "Run mode is invalid. Please use 'starter' to initiate prediction pipelie or 'piper' to continue")
           })
-
-test_that("signalp outputs a valid result object",
-          {
-            
-          })
