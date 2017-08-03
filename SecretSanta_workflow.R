4abef2f68b3d759b40a56239d8326ed0ea032d88
uselessylusses <gogleva.a.a@gmail.com>
2017-08-03 10:46:48 +0100

    to do: turn this into a unit test

--- SecretSanta_workflow.R
+++ SecretSanta_workflow.R
@@ -1,6 +1,7 @@
 ### SecretSanta workflow----
 ### Install signalPs (multiple versions if required) -----
 ### Put paths in the separate tab-delimeted file and upload them using manage_paths()----
+### TO DO: turn this into unit test!
 secret_paths <- manage_paths("SecretSanta/inst/extdata/sample_paths") #this will leave in the environment
 
 ### Example pipe pipeline:
