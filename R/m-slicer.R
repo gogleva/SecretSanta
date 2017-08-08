435124d087d13e2e36c2c97e08ae3330607b1833
uselessylusses <gogleva.a.a@gmail.com>
2017-08-08 14:18:25 +0100

    function template for m-slicer

---
+++ R/m-slicer.R
@@ -0,0 +1,16 @@
+#' m-slicer function
+#'
+#' This function returns next most probable translation start site
+#' 
+#' @param input_object    an instance of CBSResult class or AAStringSet class containing protein sequences as on of the attributes
+#' 
+#' @export
+#' @examples 
+
+
+m-slicer <- function() {
+            pass
+        }
+
+
+
