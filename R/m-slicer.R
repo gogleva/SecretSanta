49d9d191966b02acb25fc74ac42e0e1e542e3424
uselessylusses <gogleva.a.a@gmail.com>
2017-08-08 17:13:03 +0100

    slightly better, but does not work for lists with a single element

--- R/m-slicer.R
+++ R/m-slicer.R
@@ -12,7 +12,8 @@
 #' aa <- readAAStringSet(system.file("extdata", "sample_prot_100.fasta", package = "SecretSanta"), use.names = TRUE)
 #' 
 
-m_slicer <- function(input_object, length_threshold, n) {
+m_slicer <- function(input_object, length_threshold) {
+                    input_object <- aa
                     mi <- vmatchPattern('M', input_object)
                     smi <- startIndex(mi) #all M-positions
                     fifi <- function(x) { unlist(x)[unlist(x) >1]}
@@ -29,32 +30,33 @@ m_slicer <- function(input_object, length_threshold, n) {
                       names(st) <- paste(unlist(strsplit(names(st), ' '))[1], '_slice_M', x, sep = '')
                       if (width(st) >= length_threshold) {return (st)}
                                 }
-                                              }
-  
+                      
                     # one AAStringSet object:
-                    #slices <- do.call(c, unlist(sapply(X = unlist(smi[n]), FUN = slice, seq = input_object[n])))
-                    #return(slices)
-
-                    listA <- input_object
-                    listB <- smi
                     
-                    mapply(function(X,Y) {
-                                          sapply(1:length(X), function(x) )
-                    })
-                    
-                    many_slices <- function(x,y) {
-                      result <- unlist(sapply(X = unlist(x), FUN = slice, seq = y))
-                      return(result)
+                    many_slices <- function(n) {
+                      
+                      do.call(c, unlist(sapply(X = unlist(smi[n]), FUN = slice, seq = input_object[n])))
                     }
                     
+                    smt <- sapply(1:length(smi), many_slices)
+                    return(smt)
                     
-                    listA <- list(matrix(rnorm(2000), nrow=10),
-                                  matrix(rnorm(2000), nrow=10))
-                    listB <- list(matrix(rnorm(2000), nrow=10),
-                                  matrix(rnorm(2000), nrow=10))
-                    mapply(function(X,Y) {
-                      sapply(1:10, function(row) cor(X[row,], Y[row,]))
-                    }, X=listA, Y=listB)
-
-  
         }
+
+
+l1 <- list(a = c(1:10), b = c(11:20))
+l2 <- list(c = c(21:30), d = c(31:40))
+# sum the corresponding elements of l1 and l2
+mapply(sum, l1$a, l1$b, l2$c, l2$d)
+
+listA <- input_object
+listB <- smi
+
+mapply(slice, listA[1], unlist(listB[1])
+    
+test_function <- function(n) {return(n + 10)}       
+       
+       
+sapply(X = c(1:10), test_function)
+
+       
