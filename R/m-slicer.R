4da7b2f6d317af191a2ecb24e375a479dbbd87b2
uselessylusses <gogleva.a.a@gmail.com>
2017-08-08 14:41:40 +0100

    function concept updated

--- R/m-slicer.R
+++ R/m-slicer.R
@@ -1,16 +1,20 @@
 #' m-slicer function
 #'
-#' This function returns next most probable translation start site
+#' Experimental option
+#' This function generates all possible subsequences starting with M.
+#' Assumption: translation start sites might be mis-predicted in the original set of proteins.
+#' Output of this step can be used as an iput for secretome prediction pipeline
 #' 
-#' @param input_object    an instance of CBSResult class or AAStringSet class containing protein sequences as on of the attributes
-#' 
+#' @param input_object    an instance of CBSResult class or AAStringSet class containing protein sequences as on of the     attributes
+#' @param len_threshold   sliced sequences below this threshold will be discarded
 #' @export
 #' @examples 
 
 
-m-slicer <- function() {
-            pass
-        }
+m-slicer <- function(input_object, length_threshold) {
+              
+  
+              }
 
 
 
