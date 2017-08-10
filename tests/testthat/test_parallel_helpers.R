6bffd687877c7ac2eeadc496655b630a98f5d536
uselessylusses <gogleva.a.a@gmail.com>
2017-08-10 17:37:01 +0100

    unit tests for combine_SignalpResult

--- tests/testthat/test_parallel_helpers.R
+++ tests/testthat/test_parallel_helpers.R
@@ -10,3 +10,30 @@ test_that("test split_XStringSet",
             expect_error(split_XStringSet(large_inp, 100), 'Input string_set does not belong to XStringSet class')
           
           })
+
+test_that("combine_SignalpResult combines",
+          {
+            inp2 <- CBSResult(
+              in_fasta = readAAStringSet(system.file("extdata", "tail_prot.fasta", package = "SecretSanta")))
+            inp3 <- CBSResult(
+              in_fasta = readAAStringSet(system.file("extdata", "tail2_prot.fasta", package = "SecretSanta")))
+            inp4 <- CBSResult(
+              in_fasta = readAAStringSet(system.file("extdata", "sample_prot_100.fasta", package = "SecretSanta")))
+
+            sp1 <- signalp(input_obj = inp2, version = 2, organism_type = 'euk', run_mode = 'starter', paths = my_pa)
+            sp2 <- signalp(input_obj = inp3, version = 2, organism_type = 'euk', run_mode = 'starter', paths = my_pa)
+            sp3 <- signalp(input_obj = inp4, version = 2, organism_type = 'euk', run_mode = 'starter', paths = my_pa)
+            obj <- list(sp1, sp2, sp3)
+            combined_sp <- combine_SignalpResult(obj)
+            
+            expect_identical(sum(unlist(lapply(lapply(c(sp1, sp2, sp3), getInfasta), length))), 
+                             length(getInfasta(combined_sp)))
+            expect_identical(sum(unlist(lapply(lapply(c(sp1, sp2, sp3), getOutfasta), length))), 
+                             length(getOutfasta(combined_sp)))
+            expect_identical(sum(unlist(lapply(lapply(c(sp1, sp2, sp3), getSPtibble), nrow))), 
+                             length(getSPtibble(combined_sp)))
+            expect_identical(sum(unlist(lapply(lapply(c(sp1, sp2, sp3), getMatfasta), length))), 
+                             length(getMatfasta(combined_sp)))
+                                                 
+          })
+
