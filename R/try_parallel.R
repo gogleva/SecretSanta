48f4c3d77c3957474476bf96fa43a0654a7dcf4e
uselessylusses <gogleva.a.a@gmail.com>
2017-08-14 16:09:19 +0100

    bug: something happenes in parallel branch of signalp_parallel; non parallelised version works ok

--- R/try_parallel.R
+++ R/try_parallel.R
@@ -33,7 +33,7 @@ split_XStringSet <- function(string_set, chunk_size){
                                 
 }
 
-#' combine_SignalpResult function
+combine_SignalpResult function
 #'
 #' This function combines multiple instances of SignalpResult class, typically generated with parLapply
 #' @param arguments - a list of SignalpResult objects to be combined in one
@@ -315,14 +315,18 @@ signalp_parallel <- function(input_obj, version, organism_type, run_mode, paths,
 
 # # test run:
 
-# my_pa <- manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta"))
-# aa_1K <- readAAStringSet("/home/anna/anna/Labjournal/SecretSanta_external/test_fastas/medium_1K.fasta")
-# inp_1K <- SignalpResult(in_fasta = aa_1K)
+my_pa <- manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta"))
+aa_1K <- readAAStringSet("/home/anna/anna/Labjournal/SecretSanta_external/test_fastas/medium_1K.fasta")
+inp_1K <- SignalpResult(in_fasta = aa_1K)
 
 # aa_2K <- readAAStringSet("/home/anna/anna/Labjournal/SecretSanta_external/test_fastas/medium_2K.fasta")
 # inp_2K <- CBSResult(in_fasta = aa_2K)
 
-# sp_1K_par <- signalp_parallel(inp_1K, version = 2, organism_type = 'euk', run_mode = 'starter', paths = my_pa, truncate = T)
+sp_1K_par <- signalp_parallel(inp_1K, version = 2, organism_type = 'euk', run_mode = 'starter', paths = my_pa, truncate = T)
+
+s2_sp_1K_par <- signalp_parallel(sp_1K_par, version = 2, organism_type = 'euk', run_mode = 'piper', paths = my_pa, truncate = T)
+
+
 
 # # profile 1K input, parallel
 # microbenchmark::microbenchmark(signalp_parallel(inp_1K, version = 2, organism_type = 'euk', run_mode = 'starter', paths = my_pa), times = 1)
