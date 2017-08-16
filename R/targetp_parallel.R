c3c63a14af55a70036498613ce8c5f20336a4ba6
uselessylusses <gogleva.a.a@gmail.com>
2017-08-16 15:27:57 +0100

    found bug: missing bracket in length eveluation statement

--- R/targetp_parallel.R
+++ R/targetp_parallel.R
@@ -56,17 +56,13 @@ targetp_parallel <- function(input_object, network_type, run_mode, paths) {
   crop_names <- function(x){unlist(stringr::str_split(x, " "))[1]}
   
   # ----- Check that inputs are valid
-  
   # check that input object belong to CBSResult class
-  
   if (is(input_object, "CBSResult")) {} else {stop('input_object does not belong to CBSResult superclass')}
   
   # check that supplied runnig mode is valid
-  
   if (run_mode %in% c('piper', 'starter')) {} else {stop("Run mode is invalid. Please use 'starter' to initiate prediction pipelie or 'piper' to continue")}
   
   # check that input_object contains non-empty in/out_fasta for starter/piper
-  
   if (run_mode == 'starter') {
     if (length(getInfasta(input_object)) != 0) {
       fasta <- getInfasta(input_object)
@@ -90,26 +86,26 @@ targetp_parallel <- function(input_object, network_type, run_mode, paths) {
   
   simple_targetp <- function(aaSet){
       #----- Run targetp prediction:
-      message(paste('Number of submitted sequences...', length(fasta)))
+      message(paste('Number of submitted sequences...', length(aaSet)))
   
       # convert fasta to a temporary file:
       out_tmp <- tempfile() #create a temporary file for fasta
-      Biostrings::writeXStringSet(fasta, out_tmp) #write tmp fasta file
+      Biostrings::writeXStringSet(aaSet, out_tmp) #write tmp fasta file
   
       # get path to targetp executable
       full_pa <- as.character(paths %>% dplyr::filter(tool == 'targetp') %>% dplyr::select(path))
   
       # prep fasta:
       # generate cropped names for input fasta
-      cropped_names <- unname(sapply(names(fasta), crop_names))
+      cropped_names <- unname(sapply(names(aaSet), crop_names))
       # replace long names with cropped names
-      names(fasta) <- cropped_names
+      names(aaSet) <- cropped_names
   
       # prep networks argument:
       NN <- paste('-', network_type, sep = '')
   
       #run targetp:
-      tp <- tibble::as.tibble(read.table(text = (system(paste(full_pa, NN, out_tmp), intern = TRUE)[1: length(fasta) + 8])))
+      tp <- tibble::as.tibble(read.table(text = (system(paste(full_pa, NN, out_tmp), intern = TRUE)[1: length(aaSet) + 8])))
   
       if (network_type == 'N') {
         names(tp) <- c('gene_id', 'length', 'mTP', 'sp', 'other', 'TP_localization', 'RC')}
@@ -121,20 +117,29 @@ targetp_parallel <- function(input_object, network_type, run_mode, paths) {
       message(paste('Number of candidate secreted sequences', nrow(tp)))         
   
       candidate_ids <- tp %>% dplyr::select(gene_id) %>% unlist(use.names = FALSE)
-      out_fasta_tp <- fasta[candidate_ids]
+      out_fasta_tp <- aaSet[candidate_ids]
   
       # generate output object:
-      out_obj <- TargetpResult(in_fasta = fasta,
-                           out_fasta = out_fasta_tp,
-                           tp_tibble = tp)
+      out_obj <- TargetpResult(in_fasta = aaSet,
+                               out_fasta = out_fasta_tp,
+                               tp_tibble = tp)
   
       if (validObject(out_obj)) {return(out_obj)}
   }
   
-  
-   # Check input file size and decide how to run targetp: in parallel mode or not:
-   if (length(fasta <= 500)) {message('Ok for single processing')}
-      return(simple_targetp(fasta))
+
+   # # Check input file size and decide how to run targetp: in parallel mode or not:
+   if (length(fasta) <= 500) {
+     message('Ok for single processing')
+     return(length(fasta))
+   } else {
+     stop('too many sequences')
+   } 
+     
+   #    return(simple_targetp(fasta))
+   # } else {
+   #   stop('Input fasta contains >500 sequences, entering batch mode...')
+   # }
   
 }
 
@@ -143,6 +148,8 @@ targetp_parallel <- function(input_object, network_type, run_mode, paths) {
 
 inp_10K <- CBSResult(in_fasta = readAAStringSet("/home/anna/anna/Labjournal/SecretSanta_external/test_fastas/large_10K.fasta"))
 targetp(input_object = inp_1k, network_type = 'N', run_mode = 'starter', paths = my_pa)
+
+inp_100 <- CBSResult(in_fasta = readAAStringSet(""))
 
 
 
