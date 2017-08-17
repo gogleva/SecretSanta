1410bf4ebdec6ba9e7af7f6ab1b7af0ddc430a89
uselessylusses <gogleva.a.a@gmail.com>
2017-08-17 14:23:44 +0100

    TMHMM requires a different type of split: 3 fastas should be splitted, wrote draft simple_tmhmm function; will probably break

--- R/tmhmm_parallel.R
+++ R/tmhmm_parallel.R
@@ -40,6 +40,40 @@ combine_TMhmmResult <- function(arguments) {
 
 }
 
+
+# # prepare and split SihnlapResult object for parallel processing with TMHMM
+# 
+# prep_and_split_SignalpResult <- function(sp_object){
+#   
+#             out <- getOutfasta(sp_object)
+#             mature <- getMatfasta(sp_object)
+#                       
+# }
+# 
+
+inp_1K <- CBSResult(in_fasta = readAAStringSet("SecretSanta_external/test_fastas/medium_1K.fasta"))
+r1 <- signlap(inp_1k, version = 4, 'euk', run_mode = "starter", paths = my_pa)
+ 
+out <- getOutfasta(r1)
+mature <- getMatfasta(r1)
+
+split_out <- split_XStringSet(out, 10)
+split_mature <- split_XStringSet(mature, 10)
+
+cc <- mapply(list, split_out, split_mature, SIMPLIFY=F)
+
+#get all the 2nd elemnts:
+sapply(cc, '[', 2)
+
+#get all the 1st elements
+sapply(cc, '[', 1)
+
+
+fasta_tup <- list(out, mature)
+
+
+
+
 # parallel version of TMHMM
 
 #' tmhmm function
@@ -53,7 +87,6 @@ combine_TMhmmResult <- function(arguments) {
 #'                  predicted as TM domain, avoid this
 #' @param paths tibble with paths to external dependencies, generated with \code{\link{manage_paths}} function
 #' @param TM  allowed number of TM domains in mature peptides, recommended value <= 1; use 0 for strict filtering             
-#' @export
 #' @examples 
 #'           
 #' my_pa <- manage_paths(system.file("extdata", "sample_paths", package = "SecretSanta"))
@@ -88,18 +121,27 @@ tmhmm_parallel <- function(input_obj, paths, TM) {
   
   fasta <- getMatfasta(input_obj) 
   
+  # do file preps here
+
+  test_tripple <- list(getOutfasta(r1), getMatfasta(r1), getInfasta(r1))
+  simple_tmhmm(test_tripple)
   
-  # simple tmhmm, takes AAStringSet object as an input
+  simple_tmhmm <- function(fasta_triple) {
+  # fasta triple - a lits of 3 elements: out_fasta, mature_fasta, in_fasta
+  # (out_fasta, mature_fasta, in_fasta)
   
-  simple_tmhmm <- function() {
-  
+      
       #----- Run tmhmm
       message("running TMHMM locally...")
-  
+    
+      outf <- fasta_triple[[1]] #original out_fasta
+      matf <- fasta_triple[[2]] #original mature_fasta
+      full_fasta <- fasta_triple[[3]] #original in_fasta
+      
       out_tmp <- tempfile()
-      Biostrings::writeXStringSet(fasta, out_tmp)
+      Biostrings::writeXStringSet(matf, out_tmp)
   
-      message(paste('Number of submitted sequences...', length(fasta)))
+      message(paste('Number of submitted sequences...', length(matf)))
   
       full_pa <- as.character(paths %>% dplyr::filter(tool == 'tmhmm') %>% dplyr::select(path))
       tm <- tibble::as.tibble(read.table(text = (system(paste(full_pa, out_tmp, '--short'), intern = TRUE))))
@@ -122,10 +164,7 @@ tmhmm_parallel <- function(input_obj, paths, TM) {
   
      message(paste('Number of candidate sequences with signal peptides and 0 TM domains in mature sequence...', nrow(tm)))
   
-
-  
      #generate cropped names for input fasta
-     full_fasta <- getInfasta(input_obj)
      cropped_names <- unname(sapply(names(full_fasta), crop_names))
   
      #replace long names with cropped names
@@ -135,10 +174,10 @@ tmhmm_parallel <- function(input_obj, paths, TM) {
      candidate_ids <- tm %>% dplyr::select(gene_id) %>% unlist(use.names = FALSE)
      out_fasta_tm <- full_fasta[candidate_ids]
   
-     out_obj <- TMhmmResult(in_fasta = getOutfasta(input_obj), # original in fasta, full length proteins
+     out_obj <- TMhmmResult(in_fasta = outf, # original in fasta, full length proteins
                             out_fasta = out_fasta_tm, # out fasta, full length proteins
-                            in_mature_fasta = fasta,
-                            out_mature_fasta = fasta[candidate_ids],
+                            in_mature_fasta = matf,
+                            out_mature_fasta = matf[candidate_ids],
                             tm_tibble = tm)
   
      # clean TMP files before exiting:
