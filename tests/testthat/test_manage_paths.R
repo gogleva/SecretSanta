fd1457e615cb5f99bc00775c53068f72141fc295
uselessylusses <gogleva.a.a@gmail.com>
2017-08-30 14:56:42 +0100

    unit test for manage_paths updated

--- tests/testthat/test_manage_paths.R
+++ tests/testthat/test_manage_paths.R
@@ -2,17 +2,38 @@ context("Check external dependencies")
 
 test_that("manage_paths returns correct object and messages",
           {
-          s_file <- system.file("extdata", "sample_paths", package = "SecretSanta")
+          s_file <- system.file("extdata", "sample_paths",
+                                package = "SecretSanta")
           
-          expect_message(manage_paths(s_file), 'All paths are valid')
-          expect_message(manage_paths(s_file), 'signalp2 test run completed')
-          expect_message(manage_paths(s_file), 'signalp3 test run completed')
-          expect_message(manage_paths(s_file), 'signalp4 test run completed')
-          expect_message(manage_paths(s_file), 'targetp test run completed')
-          expect_message(manage_paths(s_file), 'tmhmm test run completed')
-          expect_message(manage_paths(s_file), 'wolfpsort test run completed')
+          # check with in_path = TRUE (assume that all dependecies should be
+          # accessible via $PATH variable)
+
+          # this test is not crucial, can be suspended
+          expect_message(manage_paths(in_path = TRUE,
+                                      'checking dependencies acessible via $PATH'))
           
-          result <- manage_paths(s_file)
-          expect_is(result, c('tbl_df', 'tbl'))
-          expect_equal(ncol(result), 3)
+          # this test is crucial
+          r1 <- manage_paths(in_path = TRUE)
+          expect_is(r1, 'list')
+          expect_true(r1["tests"] == TRUE)
+          expect_true(r1["in_path"] == TRUE)
+          expect_true(is.na(r1["path_tibble"]))
+          
+          # check with in_path = FALSE and provide in_path file
+          
+          
+          # this test is not crucial, can be suspended
+          expect_message(manage_paths(in_path = FALSE,
+                                     path_file = s_file),
+                                    'All paths are valid')
+          
+          # this test is crucial:
+          r2 <- manage_paths(in_path = FALSE,
+                             path_file = s_file)
+          
+          expect_is(r2, 'list')
+          expect_true(r2["tests"] == TRUE)
+          expect_true(r2["in_path"] == FALSE)
+          expect_is(r2$path_tibble, 'tbl')
+          expect_equal(ncol(r2$path_tibble), 3)
           })
