883848dbb67638ed935ede29d8566a30098895a9
uselessylusses <gogleva.a.a@gmail.com>
2017-08-31 18:13:24 +0100

    argument options for rn_mode

--- R/m_slicer.R
+++ R/m_slicer.R
@@ -2,22 +2,22 @@
 #'
 #' NB: this is an \strong{experimental option}.\cr
 #' \cr
-#' This function generates all possible subsequences starting with methionines
-#' (M). This might be usefull when translation start sites assumed to be
-#' mis-predicted for some of the provided proteins. For example, in cases when
-#' the set is obtained after de novo genome or transcriptome assembly.
+#' This function generates all possible subsequences (slices) starting with 
+#' methionines (M). This might be usefull when translation start sites assumed
+#' to be mis-predicted for some of the provided proteins. For example, in cases
+#' when the set is obtained after de novo genome or transcriptome assembly.
 #' \cr
 #' \cr
 #' Output of this step can be used as an input for secretome prediction
 #' pipeline to rescue secreted proteins with potentially mis-predicted start
 #' sites. Please proceed with caution.
 #' 
-#' @param input_object    an instance of CBSResult class or AAStringSet
+#' @param input_obj    an instance of CBSResult class or AAStringSet
 #'  class containing protein sequences as on of the attributes
-#' @param length_threshold   sliced sequences below this threshold will be
+#' @param min_len   sliced sequences below this threshold will be
 #'  discarded
 #' @param run_mode  \strong{slice} - to just slice input fasta, regardless
-#'  of it's origin;
+#'  of it's origin; \cr
 #' \strong{rescue} - to get proteins not predicted to be secreted on the 
 #'  initial run, generate slices; 
 #' @return a set of sliced sequences, AAStringSet object
@@ -51,7 +51,8 @@
 #'                          run_mode = 'starter',
 #'                          paths = my_pa)
 
-m_slicer <- function(input_object, length_threshold, run_mode) {
+m_slicer <- function(input_obj, min_len,
+                     run_mode = c('slice', 'rescue')) {
                     
                     # helper functions:
                     '%!in%' <- function(x,y)!('%in%'(x,y))
@@ -59,35 +60,35 @@ m_slicer <- function(input_object, length_threshold, run_mode) {
                     # check that inputs are valid - need to split validation and
                     # assignments 
                     
-                    if (is(input_object, 'AAStringSet') && 
+                    if (is(input_obj, 'AAStringSet') && 
                           (run_mode != 'slice')) {
                         stop("Please use run_mode 'slice' for an input object of AAStringSet class")
                     }
                     
-                    if (is(input_object, 'CBSResult') && (run_mode != 'rescue'))
+                    if (is(input_obj, 'CBSResult') && (run_mode != 'rescue'))
                       {
                         stop("Please use run_mode 'rescue' for an input object of CBSResult class")
                     }
 
                     # hadle inputs
                     
-                    if (is(input_object, 'AAStringSet')) {
-                    input_object <- input_object}
+                    if (is(input_obj, 'AAStringSet')) {
+                    input_obj <- input_obj}
                     
-                    if (is(input_object, 'CBSResult')) {
-                      infa <- getInfasta(input_object)
-                      outfa <- getOutfasta(input_object)
-                      input_object <- infa[names(infa) %!in% names(outfa)]
+                    if (is(input_obj, 'CBSResult')) {
+                      infa <- getInfasta(input_obj)
+                      outfa <- getOutfasta(input_obj)
+                      input_obj <- infa[names(infa) %!in% names(outfa)]
                       }
                     
-                    mi <- vmatchPattern('M', input_object)
+                    mi <- vmatchPattern('M', input_obj)
                     smi <- startIndex(mi) #all M-positions
                     fifi <- function(x) { unlist(x)[unlist(x) >1]}
 
                     # remove 1's from smi:
                     smi <- lapply(smi, fifi)
                     wi <- which(lengths(smi) > 0)
-                    input_object <- input_object[wi]
+                    input_obj <- input_obj[wi]
                     smi <- smi[lengths(smi) > 0]
 
                     # slice one AAString
@@ -105,7 +106,7 @@ m_slicer <- function(input_object, length_threshold, run_mode) {
                     many_slices <- function(n) {
                       sl <- unlist(sapply(X = unlist(smi[n]),
                                           FUN = slice,
-                                          seq = input_object[n]))
+                                          seq = input_obj[n]))
                                                }
 
                     smt <- sapply(1:length(smi), many_slices)
