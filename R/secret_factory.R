41307103ca84d76147dab11ab3dc016970dacc2d
uselessylusses <gogleva.a.a@gmail.com>
2017-11-09 13:51:52 +0000

    factory draft

---
+++ R/secret_factory.R
@@ -0,0 +1,21 @@
+#' generate proteins with alternative translation start sites
+#' turn individual function generation into function factory?
+#' write a basic function to generate a generic wrapper for 
+#' a command line tool
+#' @param method we want to use
+#' @param specs for input
+#' @param paths where to get the tool
+
+secret_factory <- function(){
+    # 1. validate inputs based on tool name
+    
+    # 2. check size of the input 
+    
+    # 3. simple method + parser
+    
+    # 4. parallel method for large iputs:
+    #   run simple method in parallel and return combined result
+}
+
+# do I need to split these 4 into 4 different factory/closure functions?
+# how to generate docs? when to generate function? how to deal with environment# s?
