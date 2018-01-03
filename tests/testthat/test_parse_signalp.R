dc15503fbbb7f97f09a30b6ef5722a6b3cf6007d
Anna Gogleva <gogleva.a.a@gmail.com>
2018-01-03 13:53:43 +0000

    re-organised and updated unit test for parse_signalp()

--- tests/testthat/test_parse_signalp.R
+++ tests/testthat/test_parse_signalp.R
@@ -11,29 +11,45 @@ test_that("signalp outputs are correctly parsed for system calls",
             con_hmm_v3 <- system(paste('signalp3 -t euk -f short -m hmm -trunc 70', s_fasta), intern = TRUE)
             con_nn_v3 <- system(paste('signalp3 -t euk -f short -m nn -trunc 70', s_fasta), intern = TRUE)
             
-            test_v_con <- function(version, hmm_in, nn_in) {
+            path_hmm_2 <- system.file("extdata",
+                                      "sample_prot_sp2_hmm_out",
+                                      package = "SecretSanta")
+            
+            path_nn_2 <- system.file("extdata",
+                                     "sample_prot_sp2_nn_out",
+                                     package = "SecretSanta")    
+            
+            path_hmm_3 <- system.file("extdata",
+                                      "sample_prot_sp3_hmm_out",
+                                      package = "SecretSanta")
+            
+            path_nn_3 <- system.file("extdata",
+                                     "sample_prot_sp3_nn_out",
+                                     package = "SecretSanta")
+            
+            test_v_con <- function(version, hmm_in, nn_in, i_type) {
             
             parse_hmm_system <-
-              parse_signalp(input = hmm_in, input_type = "system_call",
+              parse_signalp(input = hmm_in, input_type = i_type,
                             method = 'hmm', version = version)
             expect_is(parse_hmm_system, 'tbl')
             expect_equal(ncol(parse_hmm_system), 10)
             expect_true(all(parse_hmm_system$Prediction == 'Signal peptide'))
 
             expect_error(
-                parse_signalp(input = nn_in, input_type = "system_call",
+                parse_signalp(input = nn_in, input_type = i_type,
                               method = 'nn'),
                 'missing argument: version')
             
             if (version == 2) {
             expect_error(
-                parse_signalp(input = nn_in, input_type = "system_call",
+                parse_signalp(input = nn_in, input_type = i_type,
                               method = 'nn', version = version),
                 'please provide source_fasta')
             }
             
             parse_nn_system <-
-                parse_signalp(input = nn_in, input_type = "system_call",
+                parse_signalp(input = nn_in, input_type = i_type,
                               method = 'nn', version = version, source_fasta = s_fasta)
             expect_is(parse_nn_system, 'tbl')
             expect_equal(ncol(parse_nn_system), 10)
@@ -58,39 +74,18 @@ test_that("signalp outputs are correctly parsed for system calls",
             expect_false(' ' %in% parse_nn_system$gene_id)
             }
             
+            # test system calls:
             # test signalp2
-            test_v_con(2, con_hmm_v2, con_nn_v2)
-           
+            test_v_con(2, con_hmm_v2, con_nn_v2, 'system_call')
+            
             # test signalp3
-            test_v_con(3, con_hmm_v3, con_nn_v3)
+            test_v_con(3, con_hmm_v3, con_nn_v3, 'system_call')
             
-          })
-
-### update tests for files next!
-
-test_that("signalp outputs are correctly parsed for files",
-          {
-            s_path <-
-              system.file("extdata", "sample_prot_signalp2_out2",
-                          package = "SecretSanta")
-            parse_sp_path <-
-              parse_signalp(input = s_path, input_type = "path")
-            expect_is(parse_sp_path, 'tbl')
-            expect_equal(ncol(parse_sp_path), 9)
-            expect_true(all(parse_sp_path$Prediction == 'Signal peptide'))
+            # test parsing from file:
+            # test signalp2
+            test_v_con(2, path_hmm_2, path_nn_2, 'path')
             
-            #check that all the fields are present
-            fields <- c("gene_id",
-                        "Cmax",
-                        "Cpos",
-                        "Ymax",
-                        "Ypos",
-                        "Smax",
-                        "Spos",
-                        "Smean",
-                        "Prediction")
-            expect_equal(names(parse_sp_path), fields)
+            # test signalp3
+            test_v_con(3, path_hmm_3, path_nn_3, 'path')
             
-            #check that gene_ids do not contain extra spaces:
-            expect_false(' ' %in% parse_sp_path$gene_id)
-          })
+         })
