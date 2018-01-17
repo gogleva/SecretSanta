1cf71bb9d79678882db6dda7c974503a3f1b2942
Anna Gogleva <gogleva.a.a@gmail.com>
2018-01-17 14:49:03 +0000

    add check for http status, catches non-existing uniprot IDS

--- R/ask_uniprot.R
+++ R/ask_uniprot.R
@@ -1,11 +1,14 @@
 #' fetch known subcellular location from UniprotKB based on uniprot ids
 #'
-#' @param UniprotID_list  a list of UniprotIDs
+#' @param uniprotID_list  a list of uniprot IDs
 #' @export 
 #' @return a list of locations
 #' @examples 
 #' id_list <- c('P39864', 'D0N4E2', 'Q5BUB4', 'D0N381', 'B1NNT7', 'D0NP26')
-#' res <- ask_uniprot(id_list)
+#' res2 <- ask_uniprot(id_list)
+#' #try submitting a list with non-existing UniprotIDs:
+#' bad_list <- c('P39864', 'D0N4E2', 'Q5BUB4', 'D0N381', 'B1NNT7', 'D0NP2688', 'D0N4E2222')
+#' res2 <- ask_uniprot(bad_list)
 
 ask_uniprot <- function(uniprotID_list) {
     
@@ -13,29 +16,41 @@ ask_uniprot <- function(uniprotID_list) {
         base <- 'http://www.uniprot.org/uniprot/'
         fetch_url <- paste(base, uniprotID, '.txt', sep = '')
         resp <- httr::GET(fetch_url)
-        cont <- stringr::str_split(httr::content(resp, "text"),
-                                   '\n',
-                                   simplify = TRUE)
         
-        # fetch subcellular location field
-        loc_pattern <- 'SUBCELLULAR LOCATION:'
-        if (any(stringr::str_detect(cont, loc_pattern))) {
-            raw_loc <- cont[stringr::str_detect(cont, loc_pattern)]
-            loc <- stringr::str_replace(raw_loc,
-                                        'CC   -!- SUBCELLULAR LOCATION: ', '')
-        } else {
-            loc <- 'not present'
-        }
-        
-        # fetch and parse keywords
-        kw_pattern <- '^KW   '
-        if (any(stringr::str_detect(cont, kw_pattern))) {
-            raw_keys <- cont[stringr::str_detect(cont, kw_pattern)]
-            keys <- stringr::str_replace(raw_keys, 'KW   ', '') %>%
-                paste(collapse = ' ')
+        # check here for status errors:
+        if (httr::http_error(resp)) {
+            msg <- httr::http_status(resp)$message
+            keys <- loc <- msg
+            warning(paste('location retrival for provided UniprotID', uniprotID,
+                          'failed due to:', '\n', msg,
+                          '\n', 
+                          'please check the offending id', sep = ' '), call. = FALSE)
             
         } else {
-            keys <- 'not present'
+            cont <- stringr::str_split(httr::content(resp, "text"),
+                                       '\n',
+                                       simplify = TRUE)
+            
+            # fetch subcellular location field
+            loc_pattern <- 'SUBCELLULAR LOCATION:'
+            if (any(stringr::str_detect(cont, loc_pattern))) {
+                raw_loc <- cont[stringr::str_detect(cont, loc_pattern)]
+                loc <- stringr::str_replace(raw_loc,
+                                            'CC   -!- SUBCELLULAR LOCATION: ', '')
+            } else {
+                loc <- 'not present'
+            }
+            
+            # fetch and parse keywords
+            kw_pattern <- '^KW   '
+            if (any(stringr::str_detect(cont, kw_pattern))) {
+                raw_keys <- cont[stringr::str_detect(cont, kw_pattern)]
+                keys <- stringr::str_replace(raw_keys, 'KW   ', '') %>%
+                    paste(collapse = ' ')
+                
+            } else {
+                keys <- 'not present'
+            }
         }
         
         result <- tibble(UniprotID = uniprotID,
