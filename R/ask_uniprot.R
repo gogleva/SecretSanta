e4388474ff4d30611952e8edabb5599faaf69473
Anna Gogleva <gogleva.a.a@gmail.com>
2018-01-17 11:54:25 +0000

    rename file with ask_uniprot function

--- R/ask_uniprot.R
+++ R/ask_uniprot.R
@@ -5,7 +5,7 @@
 #' @return a list of locations
 #' @examples 
 #' id_list <- c('P39864', 'D0N4E2', 'Q5BUB4', 'D0N381', 'B1NNT7', 'D0NP26')
-#' known_locations <- fetch_uniprot_loc(id_list)
+#' known_locations <- ask_uniprot(id_list)
 
 ask_uniprot <- function(uniprotID_list) {
     
