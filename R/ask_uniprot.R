011a901d2796104c8eb253ff9e4afa743f737102
Anna Gogleva <gogleva.a.a@gmail.com>
2018-01-17 13:41:00 +0000

    add search for key words

--- R/ask_uniprot.R
+++ R/ask_uniprot.R
@@ -13,21 +13,35 @@ ask_uniprot <- function(uniprotID_list) {
         base <- 'http://www.uniprot.org/uniprot/'
         fetch_url <- paste(base, uniprotID, '.txt', sep = '')
         resp <- httr::GET(fetch_url)
-        cont <- stringr::str_split(httr::content(resp, "text"), '\n', simplify = TRUE)
-        loc_pattern <- 'SUBCELLULAR LOCATION:'
+        cont <- stringr::str_split(httr::content(resp, "text"),
+                                   '\n',
+                                   simplify = TRUE)
         
+        # fetch subcellular location field
+        loc_pattern <- 'SUBCELLULAR LOCATION:'
         if (any(stringr::str_detect(cont, loc_pattern))) {
             raw_loc <- cont[stringr::str_detect(cont, loc_pattern)]
-            raw_loc <- stringr::str_replace(raw_loc, 
-                                            'CC   -!- SUBCELLULAR LOCATION: ',
-                                            '')
-            
+            loc <- stringr::str_replace(raw_loc,
+                                        'CC   -!- SUBCELLULAR LOCATION: ', '')
         } else {
-            raw_loc <- 'not present'
+            loc <- 'not present'
         }
         
-        raw_loc
+        # fetch and parse keywords
+        kw_pattern <- '^KW   '
+        if (any(stringr::str_detect(cont, kw_pattern))) {
+            raw_keys <- cont[stringr::str_detect(cont, kw_pattern)]
+            keys <- stringr::str_replace(raw_keys, 'KW   ', '') %>%
+                paste(collapse = ' ')
+            
+        } else {
+            keys <- 'not present'
         }
+        
+        result <- list(loc, keys)
+    }
     
     all_res <- sapply(uniprotID_list, simple_fetch, simplify = TRUE)
 }
+
+### experiments
