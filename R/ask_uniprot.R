8439cfcc0db14bafe49c02ad4cbd354c64adfb6c
Anna Gogleva <gogleva.a.a@gmail.com>
2018-01-17 16:09:14 +0000

    add GO:CC retrival to ask_uniprot

--- R/ask_uniprot.R
+++ R/ask_uniprot.R
@@ -5,7 +5,7 @@
 #' @return a list of locations
 #' @examples 
 #' id_list <- c('P39864', 'D0N4E2', 'Q5BUB4', 'D0N381', 'B1NNT7', 'D0NP26')
-#' res2 <- ask_uniprot(id_list)
+#' res <- ask_uniprot(id_list)
 #' #try submitting a list containing some non-existing UniprotIDs:
 #' bad_list <- c('P39864', 'D0N4E2', 'Q5BUB4', 'D0N381', 'B1NNT7', 'D0NP2688', 'D0N4E2222')
 #' res2 <- ask_uniprot(bad_list)
@@ -20,7 +20,7 @@ ask_uniprot <- function(uniprotID_list) {
         # check here for status errors:
         if (httr::http_error(resp)) {
             msg <- httr::http_status(resp)$message
-            keys <- loc <- msg
+            keys <- loc <- go_cc <- msg
             warning(paste('location retrival for provided UniprotID', uniprotID,
                           'failed due to:', '\n', msg,
                           '\n', 
@@ -46,17 +46,31 @@ ask_uniprot <- function(uniprotID_list) {
             kw_pattern <- '^KW   '
             if (any(stringr::str_detect(cont, kw_pattern))) {
                 raw_keys <- cont[stringr::str_detect(cont, kw_pattern)]
-                keys <- stringr::str_replace(raw_keys, 'KW   ', '') %>%
+                keys <- stringr::str_replace(raw_keys, kw_pattern, '') %>%
                     paste(collapse = ' ')
                 
             } else {
                 keys <- 'not present'
             }
+            
+            # fetch relevant GO terms (CC)
+            go_pattern <- '^DR   GO; '
+            
+            if (any(stringr::str_detect(cont, go_pattern))) {
+                go <- cont[stringr::str_detect(cont, go_pattern)]
+                go_cc <- stringr::str_replace(go, go_pattern, '')
+                go_cc <- go_cc[grepl('C:', go_cc)] %>%
+                    paste(collapse = ' ')
+                
+            } else {
+                go_cc <- 'not present'
+            }
         }
         
         result <- tibble(UniprotID = uniprotID,
                          Subcellular.Location = loc,
-                         Key.Words = keys)
+                         Key.Words = keys,
+                         GO.CC = go_cc)
     }
     
     all_res <- dplyr::bind_rows(lapply(uniprotID_list, simple_fetch))
--- tests/testthat/test_ask_uniprot.R
+++ tests/testthat/test_ask_uniprot.R
@@ -8,7 +8,7 @@ test_that("ask_uniprot works",
            result_ok <- ask_uniprot(id_list)
            expect_is(result_ok, 'tbl_df')
            expect_equal(id_list, result_ok$UniprotID)
-           expect_equal(dim(result_ok), c(6, 3))
+           expect_equal(dim(result_ok), c(6, 4))
            
            # some bad/non-existent ids:
            bad_list <- c('P39864', 'D0N4E2', 'Q5BUB4', 'D0N381', 'B1NNT7', 'D0NP26999')
@@ -16,7 +16,7 @@ test_that("ask_uniprot works",
            result_404 <- ask_uniprot(bad_list)
            expect_is(result_404, 'tbl_df')
            expect_equal(bad_list, result_404$UniprotID)
-           expect_equal(dim(result_404), c(6, 3))
+           expect_equal(dim(result_404), c(6, 4))
            expect_identical(result_404$Subcellular.Location[6], 
                             "Client error: (404) Not Found")
            })
